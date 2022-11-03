a08537585367f4f7d4eedfad43ec144e15c998d2
Eitan03 <eitanabcabc@gmail.com>
2022-11-03 12:29:10 +0000

    added onFileExpirationTest

--- tests/onFileExpirationTest.py
+++ tests/onFileExpirationTest.py
@@ -1,9 +1,11 @@
 import unittest
 from unittest.mock import Mock, ANY
 
-from .Mocks.DatabaseMock import createDatabaseMock
+from  tests.Mocks.DatabaseMock import createDatabaseMock
 
+import os
 from onFileExpiration import fileExpired
+from config import AMOUNT_OF_FILE_PARTS
 # def	fileExpired(file_name: str,	DatabaseFactory):
 
 class onFileExpirationTest(unittest.TestCase):
@@ -19,14 +21,29 @@ class onFileExpirationTest(unittest.TestCase):
 			'delete': deleteMock
 		})
 
-		fileExpired(file_name, db)
+		fileExpired(file_name, lambda: db)
 
 		deleteMock.assert_called_once_with(file_name)
 
 	def test_fileExpired_deleted_parts(self):
-		file_parts = []
 
-		for i in range(5):
-			
-		pass
-		
+		file_name = 'file'
+		file_parts = []
+		files_path = './tests/' + file_name + '_'
+
+
+		db = createDatabaseMock({
+			'exists': Mock(return_value=True),
+			'getHash': Mock(return_value=dict([(str(i), files_path + str(i)) for i in range(5)])),
+		})
+
+		for i in range(AMOUNT_OF_FILE_PARTS):
+			file_path = files_path + str(i)
+			with open(file_path, 'w') as f:
+				f.write('123')
+
+		fileExpired(file_name, lambda: db)
+
+		for i in range(AMOUNT_OF_FILE_PARTS):
+			if os.path.isfile(files_path + str(i)):
+				self.fail(f'file {files_path + str(i)} exists')
