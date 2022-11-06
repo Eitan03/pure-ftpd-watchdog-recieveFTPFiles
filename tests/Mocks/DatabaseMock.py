48821382f9949e29252999dc3905612c85f75158
Eitan03 <eitanabcabc@gmail.com>
2022-11-06 12:52:33 +0000

    fixed merge errors

--- tests/Mocks/DatabaseMock.py
+++ tests/Mocks/DatabaseMock.py
@@ -11,11 +11,7 @@ def createDatabaseMock(database: dict):
     		'delete': Mock(return_value=True),
     		'exists': Mock(return_value=False),
     		'lenOfHash': Mock(return_value=0),
-<<<<<<< Updated upstream
-    		'flushAll': Mock(return_value=True)
-=======
     		'flushAll': Mock(return_value=True),
 			'incrementHashValueBy': Mock(return_value=True),
 			'getHashValue': Mock(return_value=100)
->>>>>>> Stashed changes
 		}, **database })
