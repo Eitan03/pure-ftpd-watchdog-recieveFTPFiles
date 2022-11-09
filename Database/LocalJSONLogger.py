65058c5fd1cdc9e2715044b5b1d00031bceb134e
Eitan03 <eitanabcabc@gmail.com>
2022-11-09 09:48:16 +0000

    added optional timestamp param to json logger

--- Database/LocalJSONLogger.py
+++ Database/LocalJSONLogger.py
@@ -6,14 +6,13 @@ class LocalJSONLogger():
 	def	__init__(self):
 		pass
 
-	def	log(self, indexName: str, doc: dict):
-		doc['@timestamp'] = str(datetime.now()).replace(' ', 'T') + 'Z'
+	def	log(self, indexName: str, doc: dict, addTimeStamp=True):
+		if addTimeStamp: doc['@timestamp'] = str(datetime.now()).replace(' ', 'T') + 'Z'
 		folder = os.path.join('./elasticLogs',indexName)
 		if not os.path.exists(folder):
 			print(f'creating folder {folder}')
 			os.mkdir(folder)
-		else:
-			print(f'folder {folder} exists!')
 		with open(os.path.join(folder, f'{indexName}-{str(datetime.now())}.json'), 'a') as f:
-			json.dump(doc, f)
+			data = json.dumps(doc)
+			f.write(data + '\n')
 		print(f'logged to elastic in index {indexName}')
